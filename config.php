569b6e40c198c03aea9028faa4b16eb9cb8bc69a
Simone Coscetti <s.coscetti@gmail.com>
2018-11-08 11:18:48 +0100

    correct version of install.php

--- config.php
+++ config.php
@@ -12,4 +12,4 @@ $dbname   = "test";
 $dsn      = "mysql:host=$host;dbname=$dbname";
 $options  = array(
               PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-            ); 
+            );
--- install.php
+++ install.php
@@ -1,19 +1,19 @@
 <?php
 
 /**
- *
- *
+ * Open a connection via PDO to create a
+ * new database and table with structure.
  *
  */
 
- require "config.php"
+require "config.php";
 
- try {
-   $connection = new PDO("mysql:host=$host", $username, $password, $options);
-   $sql = file_get_contents("data/init.sql");
-   $connection->exec($sql);
+try {
+	$connection = new PDO("mysql:host=$host", $username, $password, $options);
+	$sql = file_get_contents("data/init.sql");
+	$connection->exec($sql);
 
-   echo "Database and table users created succesfully.";
- } catch(PDOException $error) {
-   echo $sql . "<br>" . $error->getMessage();
- }
+	echo "Database and table users created successfully.";
+} catch(PDOException $error) {
+	echo $sql . "<br>" . $error->getMessage();
+}
