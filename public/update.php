de5d196f80ef45eb50152994013eaeccd0d5ad18
Simone Coscetti <s.coscetti@gmail.com>
2018-11-10 00:11:39 +0100

    right version of files
    minor errors checked on update and update-single

--- public/update.php
+++ public/update.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * List all users with a link to edit
+ */
+
 try {
   require "../config.php";
   require "../common.php";
@@ -12,14 +16,12 @@ try {
   $statement->execute();
 
   $result = $statement->fetchAll();
-
-} catch (PDOException $error) {
+} catch(PDOException $error) {
   echo $sql . "<br>" . $error->getMessage();
 }
 ?>
-
 <?php require "templates/header.php"; ?>
-
+        
 <h2>Update users</h2>
 
 <table>
@@ -35,20 +37,22 @@ try {
       <th>Edit</th>
     </tr>
   </thead>
-  <tbody>
-  <?php foreach ($result as $row) : ?>
-    <tr>
-      <td><?php echo escape($row["id"]); ?></td>
-      <td><?php echo escape($row["firstname"]); ?></td>
-      <td><?php echo escape($row["lastname"]); ?></td>
-      <td><?php echo escape($row["email"]); ?></td>
-      <td><?php echo escape($row["age"]); ?></td>
-      <td><?php echo escape($row["location"]); ?></td>
-      <td><?php echo escape($row["date"]); ?></td>
-      <td><a href="update-single.php?id=<?php echo escape($row["id"]); ?>">Edit</a></td>
-    </tr>
-  <?php endforeach; ?>
-  </tbody>
+    <tbody>
+    <?php foreach ($result as $row) : ?>
+      <tr>
+        <td><?php echo escape($row["id"]); ?></td>
+        <td><?php echo escape($row["firstname"]); ?></td>
+        <td><?php echo escape($row["lastname"]); ?></td>
+        <td><?php echo escape($row["email"]); ?></td>
+        <td><?php echo escape($row["age"]); ?></td>
+        <td><?php echo escape($row["location"]); ?></td>
+        <td><?php echo escape($row["date"]); ?> </td>
+        <td><a href="update-single.php?id=<?php echo escape($row["id"]); ?>">Edit</a></td>
+      </tr>
+    <?php endforeach; ?>
+    </tbody>
 </table>
 
 <a href="index.php">Back to home</a>
+
+<?php require "templates/footer.php"; ?>
