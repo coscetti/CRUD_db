127f9e1e7cc5c1e17e265b0d6ff88d7f5ec1ca16
Simone Coscetti <s.coscetti@gmail.com>
2018-11-09 10:27:28 +0100

    read.php OK

--- public/read.php
+++ public/read.php
@@ -28,9 +28,44 @@ if (isset($_POST['submit'])) {
   }
 }
  ?>
-
-
 <?php require "templates/header.php"; ?>
+
+<?php
+if (isset($_POST['submit'])) {
+  if ($result && $statement->rowCount() > 0) { ?>
+    <h2>Results</h2>
+
+    <table>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email Address</th>
+          <th>Age</th>
+          <th>Location</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+  <?php foreach ($result as $row) { ?>
+      <tr>
+        <td><?php echo escape($row["id"]); ?></td>
+        <td><?php echo escape($row["firstname"]); ?></td>
+        <td><?php echo escape($row["lastname"]); ?></td>
+        <td><?php echo escape($row["email"]); ?></td>
+        <td><?php echo escape($row["age"]); ?></td>
+        <td><?php echo escape($row["location"]); ?></td>
+        <td><?php echo escape($row["date"]); ?></td>
+      </tr>
+    <?php } ?>
+      </tbody>
+  </table>
+  <?php } else { ?>
+      <blockquote>No results found for <?php echo escape($_POST['location']); ?>.</blockquote>
+    <?php }
+  } ?>
+
 
 <h2> Find user based on location</h2>
 
@@ -42,4 +77,4 @@ if (isset($_POST['submit'])) {
 
 <a href="index.php">Back to home</a>
 
-<?php include "templates/footer.php"; ?>
+<?php require "templates/footer.php"; ?>
