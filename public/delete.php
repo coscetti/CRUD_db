356c6dfa5b2110f1d68d46abb96778e7f24e7255
Simone Coscetti <s.coscetti@gmail.com>
2018-11-10 00:37:59 +0100

    Update public/delete.php

--- public/delete.php
+++ public/delete.php
@@ -1,13 +1,31 @@
 <?php
 
 /**
- * List all users with a link to edit
+ * Delete a user
  */
 
-try {
-  require "../config.php";
-  require "../common.php";
+require "../config.php";
+require "../common.php";
 
+if (isset($_GET["id"])) {
+  try {
+    $connection = new PDO($dsn, $username, $password, $options);
+  
+    $id = $_GET["id"];
+
+    $sql = "DELETE FROM users WHERE id = :id";
+
+    $statement = $connection->prepare($sql);
+    $statement->bindValue(':id', $id);
+    $statement->execute();
+
+    $success = "User successfully deleted";
+  } catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
+  }
+}
+
+try {
   $connection = new PDO($dsn, $username, $password, $options);
 
   $sql = "SELECT * FROM users";
@@ -22,7 +40,9 @@ try {
 ?>
 <?php require "templates/header.php"; ?>
         
-<h2>Update users</h2>
+<h2>Delete users</h2>
+
+<?php if ($success) echo $success; ?>
 
 <table>
   <thead>
@@ -37,20 +57,20 @@ try {
       <th>Delete</th>
     </tr>
   </thead>
-    <tbody>
-    <?php foreach ($result as $row) : ?>
-      <tr>
-        <td><?php echo escape($row["id"]); ?></td>
-        <td><?php echo escape($row["firstname"]); ?></td>
-        <td><?php echo escape($row["lastname"]); ?></td>
-        <td><?php echo escape($row["email"]); ?></td>
-        <td><?php echo escape($row["age"]); ?></td>
-        <td><?php echo escape($row["location"]); ?></td>
-        <td><?php echo escape($row["date"]); ?> </td>
-        <td><a href="update-single.php?id=<?php echo escape($row["id"]); ?>">Delete</a></td>
-      </tr>
-    <?php endforeach; ?>
-    </tbody>
+  <tbody>
+  <?php foreach ($result as $row) : ?>
+    <tr>
+      <td><?php echo escape($row["id"]); ?></td>
+      <td><?php echo escape($row["firstname"]); ?></td>
+      <td><?php echo escape($row["lastname"]); ?></td>
+      <td><?php echo escape($row["email"]); ?></td>
+      <td><?php echo escape($row["age"]); ?></td>
+      <td><?php echo escape($row["location"]); ?></td>
+      <td><?php echo escape($row["date"]); ?> </td>
+      <td><a href="delete.php?id=<?php echo escape($row["id"]); ?>">Delete</a></td>
+    </tr>
+  <?php endforeach; ?>
+  </tbody>
 </table>
 
 <a href="index.php">Back to home</a>
